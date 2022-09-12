88cff4c21011bfe52c0e92c6102deada4fefa7f1
Ahmed Ali <aelzawawy45@gmail.com>
2022-09-12 17:08:45 +0200

    news app

---
+++ index.js
@@ -0,0 +1,29 @@
+const request = require("request");
+const express = require("express");
+
+
+const url = `https://newsapi.org/v2/everything?q=tesla&from=2022-08-12&sortBy=publishedAt&apiKey=295f9fadae4447d080297f03f0a64188`;
+
+// Initializing server
+const app = express();
+const port = process.env.port || 3000;
+
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
+
+// Exress public path
+app.use(express.static(__dirname));
+
+// Setting HSB
+app.set("view engine", "hbs");
+
+request({ url, json: true }, (err, res) => {
+  const data = res.body.articles;
+  app.get("/", (req, res) => {
+    res.render("index", {
+      data,
+    });
+  });
+  console.log(typeof data);
+});
